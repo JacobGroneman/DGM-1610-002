9c097c64c173edb644e2a16951df77f8ed561b2e
Jacob Groneman <jacobjazzdrummer@gmail.com>
2019-08-29 15:40:13 -0600

    Practiced C# basics with the example file: "StartBehavior"

---
+++ Project 1/Assets/Scripts/StartBehavior.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class StartBehavior : MonoBehaviour
+{
+    private void Start()
+       {
+        print("Hello World");
+        print("Hello World");
+        print("Hello World");
+    }
+
+    void Update()
+    {
+        
+    }
+}
