7920cbc62b8a62d5da4258fdd1bad1332d1ac491
Jacob Groneman <jacobjazzdrummer@gmail.com>
2019-09-06 11:18:18 -0600

    Practiced fun code in new C# 09062019 file

---
+++ Project 1/Assets/Scripts/ScriptFor09062019.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using UnityEditor.PackageManager;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.Serialization;
+
+public class ScriptFor09062019 : MonoBehaviour
+{
+    public bool theBool = false;
+    public bool theeBool = true;
+    //public pool just kidding    
+    public int integer1 = 10;
+    public float floatingSink = 10.5f;
+    //can a sink float in a public bool?
+    private float _sinkingSink = 10.9f;
+    /* can a sinking sink be a float?
+     can a sinking sink float in a public
+     bool? It might in a private one*/
+    
+    /*if private sink = floats()
+        return Error;*/
+
+    public double doubleCrust = 4;
+    public double tripleCrust = 6;
+    public string stringComments = 
+        "wow that's a butt-load of crust";
+
+    public UnityEvent pizzariaBlowsUp;
+
+    public string psaForCitizens = "String is not yarn";
+    private string sting = "sting has a private life";
+
+    public string aCoincidence =
+        "Sting is a member of The Police and police and police go on public stings";
+
+    public UnityEvent governmentConspiracy;
+    
+//Therefore the illuminati is freakin' real, bro.
+
+    public VarsClass varsClass;
+
+    public UnityEvent Event;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
