5117929f6465b4eb7b4a99a115959004eba3008d
Jacob Groneman <jacobjazzdrummer@gmail.com>
2019-09-05 21:04:49 -0600

    made another C# Script for practicing simple code (NewBehavior)

---
+++ Project 1/Assets/Scripts/NewBehaviourScript.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+using UnityEngine.Serialization;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public float floatValue = 5.5f;
+    public int integerValue = 5;
+    public bool yes = false;
+    public UnityEvent thisIsAnEvent;
+    public string stringValue = "practicing simple code";
+    public double doubleAmount = .002;
+    public decimal decimalAmount = 3;
+    
+    void Start()
+    {
+        
+    }
+
+
+    void Update()
+    {
+        
+    } 
+    //Declaring the "onTrigger" events for practice.
+    private void OnTriggerEnter(Collider other)
+{    
+    
+}
+
+    public void OnTriggerStay(Collider other)
+    {
+        
+    }
+
+    public void OnTriggerExit(Collider other)
+    {
+  
+    }
+}
