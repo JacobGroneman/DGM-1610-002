862901c4556bc6391da3e996bc7910d7db4acdf2
Jacob Groneman <jacobjazzdrummer@gmail.com>
2019-09-09 10:52:10 -0600

    practiced more simple code before Tuesday

---
+++ Project 1/Assets/Scripts/Behavior09092019.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Behavior09092019 : MonoBehaviour
+{
+    public new bool theBool = false;
+
+    public bool bool2TheSequel = true;
+
+    public int integrityInteger = 5;
+
+    public string aFact = "integer sounds kind of like integrity";
+
+    public float youllFloatToo = 27f;
+
+    public UnityEvent Event;
+
+    public string aString = "One day I might be a coding genius...";
+    
+    public long soLong = Int64.MaxValue;
+    //what the crud is a long? like a five dollar foot long? That's pretty long
+
+    private bool welcomeToMyPool = false;
+    //Lol I don't have a pool ;D
+
+    public UnityEvent anEvent;
+    
+
+    public decimal lessThan1 = Decimal.One;
+
+    public string yay = "strings are stringy";
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
