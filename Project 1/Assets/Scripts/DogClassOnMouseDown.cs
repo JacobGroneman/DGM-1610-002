80fc1e70c1e2a0b31c69edd89ec2dba8a62b6f82
Jacob Groneman <jacobjazzdrummer@gmail.com>
2019-09-12 15:21:10 -0600

    Expounded on Triggers and Events

---
+++ Project 1/Assets/Scripts/DogClassOnMouseDown.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class DogClassOnMouseDown : MonoBehaviour
+{
+    public UnityEvent doesStuff;
+    
+    private void OnMouseDown()
+    {
+      doesStuff.Invoke();  
+    }
+}
---
+++ Project 1/Assets/Scripts/TriggerEventPracticeScript.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class TriggerEventPracticeScript : MonoBehaviour
+{
+    public class TriggerEvents : MonoBehaviour
+    {
+        public UnityEvent triggerEvent;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            triggerEvent.Invoke();
+        }
+    }
+        
+}
