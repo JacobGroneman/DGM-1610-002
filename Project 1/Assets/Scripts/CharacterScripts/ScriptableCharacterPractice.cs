f182ae817ace993376ad43668b48fe1894892bea
Jacob Groneman <jacobjazzdrummer@gmail.com>
2019-09-28 23:45:22 -0600

    Working on Scriptable Object Templates Again

--- Project 1/Assets/Scripts/CharacterScripts/ScriptableCharacterPractice.cs
+++ Project 1/Assets/Scripts/CharacterScripts/ScriptableCharacterPractice.cs
@@ -8,14 +8,11 @@ using UnityEngine;
 public class ScriptableCharacterPractice : ScriptableObject
 {
    public new string name;
-
    public string description;
 
    public Sprite artwork;
 
    public int manaCost;
-
    public int attack;
-
    public int health;
 }
---
+++ Project 1/Assets/cardDisplay.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cardDisplay : MonoBehaviour
+{
+
+    public card Card;
+    void Start()
+    {
+        
+    }
+
+}
