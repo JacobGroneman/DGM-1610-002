9ec218a56859b529f6bc5fbcb989b63aa7e6df42
Jacob Groneman <jacobjazzdrummer@gmail.com>
2019-09-30 23:19:07 -0600

    Worked on a Fun DataBase of Scriptable characters Lol

---
+++ Project 1/Assets/Scripts/Scriptable Objects/NaughtyPeopleScripts/CardTemplate2.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[CreateAssetMenu(fileName = "JailBird", menuName = "Naughty Person")]
+public class CardTemplate2 : ScriptableObject
+{
+    public new string name;
+    public string description;
+    public Sprite mugshot;
+
+    public string favoritePastTime;
+    public string paraphernalia;
+    public string victim;
+    
+
+    public void Print()
+    {
+        Debug.Log(name + "is arrested for" + description + "." + "His favorite pastime is:" + favoritePastTime + "." +
+                  "the victim was:" + victim + ".");
+
+    }
+   
+
+}
---
+++ Project 1/Assets/convictDisplay.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class convictDisplay : MonoBehaviour
+{
+    public CardTemplate2 convictStats;
+    
+    public Text uiConvictName;
+    public Text uiDescription;
+    public Image uiMugShot;
+    public Text uiFavoritePastTime;
+    public Text uiParaphernalia;
+    public Text uiVictimName;
+    
+    void Start()
+    {
+        uiConvictName.text = convictStats.name;
+        uiDescription.text = convictStats.description.ToString();
+        uiFavoritePastTime.text = convictStats.favoritePastTime.ToString();
+        uiParaphernalia.text = convictStats.paraphernalia.ToString();
+        uiVictimName.text = convictStats.victim.ToString();
+        uiMugShot.sprite = convictStats.mugshot;
+        
+    }
+
+    
+}
