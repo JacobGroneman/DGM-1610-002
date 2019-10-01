a7a6442004804ad4ecf771736c54da46b02925c1
Jacob Groneman <jacobjazzdrummer@gmail.com>
2019-10-01 15:58:05 -0600

    Learned how to Link ui to ScriptData

---
+++ Project 1/Assets/Scripts/Scriptable Objects/Integer Datas/intData.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+
+
+
+[CreateAssetMenu]
+public class intData : ScriptableObject
+{
+    public int value = 0;
+
+    public void UpdateValue(int number)
+    {
+        value += number;
+    }
+}
---
+++ Project 1/Assets/Scripts/Scriptable Objects/Integer Datas/uiTextController.cs
@@ -0,0 +1,28 @@
+﻿
+using System.Net.Mime;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.UI;
+using UnityEngine.XR.WSA.Input;
+
+[RequireComponent(typeof(Text))]
+public class uiTextController : MonoBehaviour
+{
+    
+    public UnityEvent startEvent;
+    
+    private Text textLabel;
+
+    public void UpdateText(intData data)
+    {
+        textLabel.text = data.value.ToString();
+    }
+    
+    void Start()
+    {
+        textLabel = GetComponent<Text>();
+        startEvent.Invoke();
+    } 
+    
+    
+}
