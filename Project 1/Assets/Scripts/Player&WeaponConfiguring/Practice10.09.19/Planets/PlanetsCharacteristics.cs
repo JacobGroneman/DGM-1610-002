dda9baa318d2468a01d87147836a0d03afe25227
Jacob Groneman <jacobjazzdrummer@gmail.com>
2019-10-09 23:26:18 -0600

    Working on Character instantiating
    "Planets and Their Moons"

---
+++ Project 1/Assets/Scripts/Player&WeaponConfiguring/Practice10.09.19/Planets/PlanetsCharacteristics.cs
@@ -0,0 +1,20 @@
+﻿
+using UnityEngine;
+
+[CreateAssetMenu]
+public class PlanetsCharacteristics : SolarSystemObjects
+{
+   public int planetNumber;
+   public bool rings = false;
+   public bool gasPlanet = false;
+   
+   public MoonsCharacteristics moon;
+
+   public void InstancePlanet()
+   {
+      var newPlanet = Instantiate(prefab);
+      var newSprite = newPlanet.GetComponentInChildren<SpriteRenderer>();
+      newSprite.sprite = sprite;
+      newSprite.color = atmosphere;
+   }
+}
---
+++ Project 1/Assets/Scripts/Player&WeaponConfiguring/Practice10.09.19/SolarSystemObjects.cs
@@ -0,0 +1,12 @@
+﻿
+using System;
+using UnityEngine;
+
+public class SolarSystemObjects : ScriptableObject
+{
+    public new string name;
+    public String surfaceArea;
+    public Color atmosphere;
+    public Sprite sprite;
+    public GameObject prefab;
+}
