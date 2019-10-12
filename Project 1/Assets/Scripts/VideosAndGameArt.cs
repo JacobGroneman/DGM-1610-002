278a5a4ec348272fc3700613c7b7d76dcd32d19d
Jacob Groneman <jacobjazzdrummer@gmail.com>
2019-10-11 23:36:22 -0600

    Watched Tutorials for Pixel Art and Unite Conference

---
+++ Project 1/Assets/Scripts/VideosAndGameArt.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VideosandGameArt : MonoBehaviour
+{
+
+    /*I watched the following tutorials for
+     Game Art and Data Scripts for next week:
+    
+    Unite Austin 2017 - Game Architecture with Scriptable Objects
+    https://www.youtube.com/watch?v=raQ3iHhE_Kk
+    
+    HOW TO MAKE SPRITE SHEETS FOR YOUR UNITY GAME - TUTORIAL
+    https://www.youtube.com/watch?v=ou8VkQB2sos
+    
+    HOW TO ANIMATE PIXEL ART GAME CHARACTERS IN PS - TUTORIAL
+    https://www.youtube.com/watch?v=mnJb5iwYAmg
+*/
+    
+    
+}
+
