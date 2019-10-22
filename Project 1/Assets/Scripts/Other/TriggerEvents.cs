b48601970488bc83752175d8f5aad1d7fca24b36
Jacob Groneman <jacobjazzdrummer@gmail.com>
2019-10-22 17:34:47 -0600

    Learned Object Lists For Data Objects

--- Project 1/Assets/Scripts/Other/TriggerEvents.cs
+++ Project 1/Assets/Scripts/Other/TriggerEvents.cs
@@ -3,8 +3,14 @@ using System;
 using UnityEngine;
 using UnityEngine.Events;
 
+[RequireComponent(typeof(Collider))]
 public class TriggerEvents : MonoBehaviour
 {
+    private void Awake()
+    {
+        GetComponent<Collider>().isTrigger = true;
+    }
+    
     public UnityEvent triggerEvent;
     private void OnTriggerEnter(Collider other)
     {
