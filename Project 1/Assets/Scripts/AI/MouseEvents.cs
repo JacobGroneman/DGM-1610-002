10610a29ebe4056bef20119a2b73048e82df842d
Jacob Groneman <jacobjazzdrummer@gmail.com>
2019-11-05 16:03:27 -0700

    Kinda Learned AI In Class

---
+++ Project 1/Assets/Scripts/AI/MouseEvents.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseEvents : MonoBehaviour
+{
+  private Camera cam;
+  public Vector3Data data;
+  
+  private void Start()
+  {
+    cam = Camera.main;
+  }
+
+  private void OnMouseDown()
+  {
+    //RaycastHit hit;
+    var ray = cam.ScreenPointToRay(Input.mousePosition);
+    
+    if (Physics.Raycast(ray, out var hit))
+    {
+      data.value = hit.point;
+    }
+    //var hit = ray.GetPoint(10);
+    //data.value = hit;
+    
+    Debug.Log(hit.point);
+  }
+  
+  
+}
