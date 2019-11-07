a7ba91c3ee90491e024eefb3f19e0ed4d6ea9da5
Jacob Groneman <jacobjazzdrummer@gmail.com>
2019-11-07 15:42:27 -0700

    Practiced AI with Brackeys

---
+++ Project 1/Assets/Scripts/AI/Controllers/EnemyStats.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/*public class EnemyStats : PlayerData
+{
+   public override void Die()
+   {
+     base.Die();
+     
+     Destroy(gameObject);
+     
+   }
+}
+*/
