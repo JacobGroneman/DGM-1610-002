cead7793f3de09817a0168098f7c7b7b82ceb846
Jacob Groneman <jacobjazzdrummer@gmail.com>
2019-11-12 15:52:44 -0700

    Learned Coroutines

---
+++ Project 1/Assets/Scripts/CodeRoutines.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CodeRoutines : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke(nameof(Counter1), 1f);
+        Invoke(nameof(Counter2), 1f);
+        Invoke(nameof(Counter3), 1f);
+    }
+
+
+    void Counter1()
+    {
+        Debug.Log(3);
+    }
+    
+    void Counter2()
+    {
+        Debug.Log(2);
+    }
+    
+    void Counter3()
+    {
+        Debug.Log(1);
+    }
+    
+    
+    private void StartLater()
+    {
+        Debug.Log(3);
+    }
+}
---
+++ Project 1/Assets/Scripts/Coroutines.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Coroutines : MonoBehaviour
+{
+    public UnityEvent startEvent, repeatEvent, endEvent;
+    private float seconds = 10f;
+    public int counter = 10;
+    private WaitForSeconds wfsobj;
+
+    private void Awake()
+    {
+        wfsobj =  new WaitForSeconds(seconds);
+    }
+
+    public void CallCoroutine()
+    {
+        StartCoroutine(RunCoroutine());
+    }
+
+
+
+    IEnumerator RunCoroutine()
+    {
+        startEvent.Invoke(); 
+        
+        while (counter > 0)
+            
+        {
+            repeatEvent.Invoke();
+            yield return wfsobj;
+            counter--;
+        }
+
+        while (canRun)
+        {
+            repeatEvent.Invoke();
+            yield return wfsobj;
+            counter--;
+        }
+        
+
+        endEvent.Invoke();
+    }
+}
