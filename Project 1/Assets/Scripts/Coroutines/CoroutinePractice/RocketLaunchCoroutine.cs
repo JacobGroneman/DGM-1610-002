496f812e21d6c2dd5683a3ef7475f6e58b2d1ba8
Jacob Groneman <jacobjazzdrummer@gmail.com>
2019-11-19 15:54:03 -0700

    Learned Animation Basics

--- Project 1/Assets/Scripts/Coroutines/CoroutinePractice/RocketLaunchCoroutine.cs
+++ Project 1/Assets/Scripts/Coroutines/CoroutinePractice/RocketLaunchCoroutine.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -6,45 +7,45 @@ using UnityEngine.Events;
 public class RocketLaunchCoroutine : MonoBehaviour
 {
     IEnumerator InitializeLaunch()
-   {
-      print("initializing the launch to Mars");
+        {
+            print("initializing the launch to Mars");
 
-      yield return StartCoroutine(FlushFuel(100f));
+            yield return StartCoroutine(FlushFuel(100f));
       
-      print("Fuel is primed");
+            print("Fuel is primed");
 
-      yield return StartCoroutine(ThrustEngines(true));
+            yield return StartCoroutine(ThrustEngines(true));
       
-      print("Looks like we're gettin lit boyz");
+            print("Looks like we're gettin lit boyz");
 
-      yield return StartCoroutine(CountdownToLiftoff(10));
+            yield return StartCoroutine(CountdownToLiftoff(10));
       
-      print("we have liftoff! We have liftoff! All the birds" +
-            "by the rocket are obliterated! The Crocodiles are blind!");
-   }
+            print("we have liftoff! We have liftoff! All the birds" +
+                  "by the rocket are obliterated! The Crocodiles are blind!");
+        }
 
-   IEnumerator FlushFuel(float fuelPercent)
-   {
-       yield return new WaitForSeconds(fuelPercent);
-       print("Filling up the Fuel Tanks, yo");
-   }
+        IEnumerator FlushFuel(float fuelPercent)
+        {
+            yield return new WaitForSeconds(fuelPercent);
+            print("Filling up the Fuel Tanks, yo");
+        }
 
-   IEnumerator ThrustEngines(bool thrusted)
-   {
-       yield return thrusted = true;
-       print("These rockets are thrusting to the max!");
-   }
+        IEnumerator ThrustEngines(bool thrusted)
+        {
+            yield return thrusted = true;
+            print("These rockets are thrusting to the max!");
+        }
 
-   IEnumerator CountdownToLiftoff(int count)
-   {
-       yield return new WaitForSeconds(count);
+        IEnumerator CountdownToLiftoff(int count)
+        {
+            yield return new WaitForSeconds(count);
        
-       while (count > 0)
-       {
-           count--;
-           print(count);
-       }
+            while (count > 0)
+            {
+                count--;
+                print(count);
+            }
        
-       print("counting down this countdown, homeslice!");
-   }
+            print("counting down this countdown, homeslice!");
+        }
 }
