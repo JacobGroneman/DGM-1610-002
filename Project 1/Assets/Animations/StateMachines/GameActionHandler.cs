4010a82baaa364cf9f71b96e635ac1c3d1b7ada0
Jacob Groneman <jacobjazzdrummer@gmail.com>
2019-11-21 15:53:03 -0700

    Learned About Animation States Yo

---
+++ Project 1/Assets/Animations/StateMachines/GameActionHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class GameActionHandler : MonoBehaviour
+{
+    public UnityEvent handlerEvent;
+    public GameAction gameActionObj;
+    
+    void Start()
+    {
+        gameActionObj.action = RaiseHandler;
+    }
+
+    public void RaiseHandler()
+    {
+        handlerEvent.Invoke();
+    }
+    
+}
