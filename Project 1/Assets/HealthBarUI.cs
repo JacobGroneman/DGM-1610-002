e17d39efe017746f253c6df664b59a4d71bab6c8
Jacob Groneman <jacobjazzdrummer@gmail.com>
2019-12-09 12:30:45 -0700

    More Final Stuffs

---
+++ Project 1/Assets/HealthBarUI.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarUI : MonoBehaviour
+{
+    private Image healthBar;
+    private float maxHealth = 100f;
+    public static float health;
+
+    private void Start()
+    {
+        healthBar = GetComponent<Image>();
+        health = maxHealth;
+    }
+
+    private void Update()
+    {
+        healthBar.fillAmount = health / maxHealth;
+    }
+}
---
+++ Project 1/Assets/colliderenter.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+
+public class colliderenter : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public UnityEvent TriggerEvent;
+    private void OnTriggerEnter(Collider other)
+    {
+        TriggerEvent.Invoke();
+    }
+    
+}
